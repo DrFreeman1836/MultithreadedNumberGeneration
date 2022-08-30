01833a41922be10511feaf5e602f65b6cfd1f194
Agafonov <hWN-h2z-aPE-8dQ>
2022-08-30 16:43:16 +0300

    bags

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,17 +1,30 @@
+import dao.NumberDaoManager;
+import dao.impl.NumberDao;
+import model.Number;
+
+import java.util.Date;
+
 public class Application {
 
   public static void main(String[] args) {
 
-//    Number n = Number.builder().number(100).date(new Date()).build();
-//
-//    Number n2 = Number.builder().number(100).date(new Date()).build();
-//
-//    NumberManager manager = new NumberDao();
-//
-//    manager.save(n);
-//    manager.save(n2);
+    Number n = Number.builder().number(100).date(new Date()).build();
+
+    Number n2 = Number.builder().number(100).date(new Date()).build();
+
+    NumberDaoManager manager = new NumberDao();
+
+    manager.save(n);
+    manager.save(n2);
 
 
+
+
+    try {
+      Thread.sleep(2000000000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
 
 
   }
